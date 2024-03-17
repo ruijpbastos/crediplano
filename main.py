007a8c0c0ca1e62569c87dc13077e23983344f4d
unknown <Rui Bastos>
2024-03-17 23:14:20 +0000

    v0.2

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Crediplano")
 
-valor = st.number_input("Valor em dívida")
-meses = st.slider("Quantos meses faltam?", 1, 480)
-spread = st.slider("Qual o spread?", 0.0, 5.0)/100
-juros = st.slider("Qual a taxa de juro?", 0.0, 10.0)/100
+valor = st.sidebar.number_input("Valor em dívida", value=200000)
+meses = st.sidebar.number_input("Quantos meses faltam?", value=480)
+spread = st.sidebar.number_input("Qual o spread?", value=1.0)/100
+juros = st.sidebar.number_input("Qual a taxa de juro?", value=1.0, step=0.001, format="%5.3f")/100
 
 def monthly_fee(annual_fee, debt):
 	return debt*annual_fee/12
@@ -17,6 +18,28 @@ def monthly_payment(debt, periods, spread, fee):
 mfee = monthly_fee(spread+juros, valor)
 mpay = monthly_payment(valor, meses, spread, juros)
 
-st.write("Prestação: ", mpay)
-st.write("Juros: ", mfee)
-st.write("Amortização: ", mpay-mfee)
+st.write("Prestação: {:.2f}".format(mpay))
+st.write("Juros: {:.2f}".format(mfee))
+st.write("Amortização: {:.2f}".format(mpay-mfee))
+
+months = [i for i in range(1, meses+1)]
+
+m_fee = []
+m_pay = []
+m_debt = []
+
+for m in range(1, meses+1):
+	mfee = monthly_fee(spread+juros, valor)
+	m_amort = mpay-mfee
+	valor = valor - m_amort
+	m_debt.append(valor)
+	m_fee.append(mfee)
+	m_pay.append(mpay)
+
+dict_df = {"Mês": months, "Prestação": m_pay, "Juros": m_fee, "Dívida": m_debt}
+df = pd.DataFrame(dict_df)
+
+df["Amortização"] = df["Prestação"] - df["Juros"]
+df = df[["Mês", "Prestação", "Juros", "Amortização", "Dívida"]]
+
+st.dataframe(df.round(2))
